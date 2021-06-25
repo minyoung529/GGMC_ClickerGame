a0ca6face2133705c86cd59ee50f6c29b01bf733
minyoung529 <77655318+minyoung529@users.noreply.github.com>
2021-06-25 10:31:15 +0900

    상점 준비

--- Assets/Scripts/Main/InstButtons.cs
+++ Assets/Scripts/Main/InstButtons.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InstButtons : MonoBehaviour
 {
     private string btnName;
+    private int money;
+    private int popular;
+    private int moneyPS;
+    private Sprite instSprite;
+    private string info;
 
     public Instrument inst;
 
     [SerializeField]
     GameObject BuyPopup;
+    [SerializeField]
+    Text nameText, infoText, detailText, moneyText;
+    [SerializeField]
+    Image instImage;
 
     void Start()
     {
@@ -25,12 +35,23 @@ public class InstButtons : MonoBehaviour
     {
         this.inst = instData;
         this.btnName = instData.instName;
+        this.money = instData.money;
+        this.popular = instData.popular;
+        this.moneyPS = instData.moneyPerSec;
+        this.instSprite = instData.instSprite;
+        this.info = instData.info;
     }
 
     public void OnClickBuy()
     {
         Debug.Log(btnName);
         BuyPopup.SetActive(true);
+
+        nameText.text = string.Format("{0}", btnName);
+        infoText.text = string.Format("{0}", info);
+        detailText.text = string.Format("5ÃÊ´ç È¹µæ µ·: {0}¿ø\nÀÎ±âµµ +{1}¿ø", moneyPS, popular);
+        moneyText.text = string.Format("{0}¿ø", money);
+        instImage.sprite = instSprite;
     }
 
     public void ReturnName()
--- Assets/Scripts/Main/InstrumenSO/InstrumentSO.cs
+++ Assets/Scripts/Main/InstrumenSO/InstrumentSO.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class Instrument
 {
     public string instName;
-    public Sprite sprite;
+    public Sprite instSprite;
     public int money;
     public int moneyPerSec;
     public int popular;
+    [TextArea] public string info;
 }
 
 [CreateAssetMenu(fileName = "InstrumentSO", menuName = "Sprictable Object/InstrumentSO")]
